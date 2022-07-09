4b30b5908748f1a8278106c19136e170f7fc715a
shioandghuladze <thegwfg@gmail.com>
2022-07-09 13:01:43 +0400

    Initial Commit

---
+++ HM14(shio andghuladze)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HM14(shio andghuladze)
+//
+//  Created by IMAC on 09.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
